796c283f0412e387c461415a353492f3fb5317a6
AlloyFrewster <73283787+AlloyFrewster@users.noreply.github.com>
2020-11-29 21:07:32 +0000

    ngoesgnsgphen

--- Module5Solution/js/script.js
+++ Module5Solution/js/script.js
@@ -3,8 +3,8 @@ $(function () { // Same as document.addEventListener("DOMContentLoaded"...
   // Same as document.querySelector("#navbarToggle").addEventListener("blur",...
   $("#navbarToggle").blur(function (event) {
     var screenWidth = window.innerWidth;
-    if (screenWidth < 769) {
-      $("#navbarSupportedContent").collapse('hide');
+    if (screenWidth < 768) {
+      $("#collapsable-nav").collapse('hide');
     }
   });
 });
@@ -80,11 +80,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 // *** start ***
 // On first load, show home view
-showLoading("#yes");
+showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
   buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  true); // Explicitly setting the flag to get JSON from server processed into an object literal
+  true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -101,10 +101,8 @@ function buildAndShowHomeHTML (categories) {
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-       var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
-      
-      
-
+      // var chosenCategoryShortName = ....
+      var chosenCategoryShortName = chooseRandomCategory(categories).short_name;
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -118,17 +116,17 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
+      // var homeHtmlToInsertIntoMainPage = ....
       chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
-      
-
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
-      insertHtml("#yes", homeHtmlToInsertIntoMainPage); 
+      insertHtml('#main-content', homeHtmlToInsertIntoMainPage);
+
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
 }
@@ -146,7 +144,7 @@ function chooseRandomCategory (categories) {
 
 // Load the menu categories view
 dc.loadMenuCategories = function () {
-  showLoading("#yes");
+  showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     allCategoriesUrl,
     buildAndShowCategoriesHTML);
@@ -156,7 +154,7 @@ dc.loadMenuCategories = function () {
 // Load the menu items view
 // 'categoryShort' is a short_name for a category
 dc.loadMenuItems = function (categoryShort) {
-  showLoading("#yes");
+  showLoading("#main-content");
   $ajaxUtils.sendGetRequest(
     menuItemsUrl + categoryShort,
     buildAndShowMenuItemsHTML);
@@ -181,7 +179,7 @@ function buildAndShowCategoriesHTML (categories) {
             buildCategoriesViewHtml(categories,
                                     categoriesTitleHtml,
                                     categoryHtml);
-          insertHtml("#yes", categoriesViewHtml);
+          insertHtml("#main-content", categoriesViewHtml);
         },
         false);
     },
@@ -237,7 +235,7 @@ function buildAndShowMenuItemsHTML (categoryMenuItems) {
             buildMenuItemsViewHtml(categoryMenuItems,
                                    menuItemsTitleHtml,
                                    menuItemHtml);
-          insertHtml("#yes", menuItemsViewHtml);
+          insertHtml("#main-content", menuItemsViewHtml);
         },
         false);
     },
@@ -347,3 +345,4 @@ function insertItemPortionName(html,
 global.$dc = dc;
 
 })(window);
+
